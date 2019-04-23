323fad792b9d2f67cb0cecb34ef1db472a4cb96a
chrisjooo <christianchrisjo@gmail.com>
2019-04-23 11:24:28 +0700

    added datatype CharInfo

---
+++ src/main/java/itb/sister/crdt/models/CharInfo.java
@@ -0,0 +1,37 @@
+package itb.sister.crdt.models;
+
+public class CharInfo {
+    private char value;
+    private String siteId;
+    private int[] positions;
+
+    public CharInfo(char value, String siteId,int[] positions){
+        this.value = value;
+        this.siteId = siteId;
+        this.positions = positions;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public String getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(String siteId) {
+        this.siteId = siteId;
+    }
+
+    public int[] getPositions() {
+        return positions;
+    }
+
+    public void setPositions(int[] positions) {
+        this.positions = positions;
+    }
+}
