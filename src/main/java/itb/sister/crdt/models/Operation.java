5b5a6c3fe8b2f0d803a3d37b0a75bb4480bf9693
chrisjooo <christianchrisjo@gmail.com>
2019-04-23 11:25:13 +0700

    added datatype Operation

---
+++ src/main/java/itb/sister/crdt/models/Operation.java
@@ -0,0 +1,47 @@
+package itb.sister.crdt.models;
+
+public class Operation {
+    private CharInfo data;
+    private String siteId;
+    private boolean operationType;
+    private int operationCount;
+
+    public Operation(CharInfo data, String siteId, boolean operationType, int operationCount){
+        this.data = data;
+        this.siteId = siteId;
+        this.operationType = operationType;
+        this.operationCount = operationCount;
+    }
+
+    public CharInfo getData() {
+        return data;
+    }
+
+    public void setData(CharInfo data) {
+        this.data = data;
+    }
+
+    public String getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(String siteId) {
+        this.siteId = siteId;
+    }
+
+    public boolean isOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(boolean operationType) {
+        this.operationType = operationType;
+    }
+
+    public int getOperationCount() {
+        return operationCount;
+    }
+
+    public void setOperationCount(int operationCount) {
+        this.operationCount = operationCount;
+    }
+}
